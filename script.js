97acab20a3d00250a1d1c8ef84967b3978f102af
Jarl Grande <jarl.grande@gmail.com>
2022-04-10 19:33:24 -0700

    added statements to password length

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
     // 1. Prompt the user for the password criteria
     //      a. Password lenght 8 < 128
+
+
 var inputPasswordLength = function() {
 
   var passwordLength = window.prompt("Please specify how many characters between 8 and 128 for your password length.");
+  var decimalCheck = (passwordLength - Math.floor(passwordLength));
 
-    // 
-    if (passwordLength === "" || passwordLength === null || passwordLength < 8 || passwordLength > 128)
-    {
-      window.alert("Invalid response! Please enter a value between 8 and 128.")
-      return inputPasswordLength();
-    }
+  if (passwordLength === null ) {
+     window.alert("Invalid response! Please enter a value between 8 and 128.");
+     return inputPasswordLength();
+
+  }
+
+  else if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Invalid response! Please enter a value between 8 and 128.")
+    return inputPasswordLength();
+
+  }
+     
 }
 inputPasswordLength();
 
