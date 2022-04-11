86eba92d7dca19e198fab95406867a495006b1dc
Jarl Grande <jarl.grande@gmail.com>
2022-04-10 19:41:21 -0700

    added general  variables

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
     // 1. Prompt the user for the password criteria
     //      a. Password lenght 8 < 128
-
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var lowercaseLetters = alphabet.split('');
+let uppercaseLetters = alphabet.toUpperCase();
+uppercaseLetters = uppercaseLetters.split('');
+var numbers = '123456789'.split('');
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 var inputPasswordLength = function() {
 
